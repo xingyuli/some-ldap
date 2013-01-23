f5004a0e7d60a67272d0bc5b0b67f215b08f8db1
Liu Xingyu <xingyulliiuu@gmail.com>
2013-01-23 14:55:13 +0800

    Copyright added.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ swordess-ldap
 =============
 
 An ODM(i.e., object-directory mapping) project which supports lazy loading and commit effective changes.
+
+Contact Me: xingyulliiuu@gmail.com or xingyu_liu@qq.com
--- src/main/org/swordess/ldap/util/DnUtils.java
+++ src/main/org/swordess/ldap/util/DnUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Swordess-ldap, an Object-Directory Mapping tool. 
+ * 
+ * Copyright (c) 2013, 2013 Liu Xingyu.
+ * 
+ * Swordess-ldap is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Swordess-ldap is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Swordess-ldap. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.swordess.ldap.util;
 
 /**
